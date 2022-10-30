02d98a150af796377256ccd6e234d65b89d988d8
Jeremy Ho <jeremyho999@gmail.com>
2022-10-30 14:24:52 -0600

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -10,7 +10,7 @@ if (cold) {
 } else {
   console.log("Short sleeves are fine.");
 }
-*/
+
 
 const temperature = 15;
 
@@ -23,3 +23,21 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+*/
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
